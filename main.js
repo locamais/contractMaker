ee081a8876dd296e38e30611fab78ea6c9603d8b
locmais <locmais.locadorajk@gmail.com>
2023-11-21 11:00:34 -0300

    first commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use('/', routes)
 
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, ()=>{
     console.log(`server on, port: ${port}`)
